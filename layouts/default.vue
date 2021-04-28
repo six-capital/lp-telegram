08c0df9ca551e281100b84536e6b80560d1f419d
Six Capital <dev@sixlaunch.com.br>
2021-04-28 14:46:23 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app light>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
